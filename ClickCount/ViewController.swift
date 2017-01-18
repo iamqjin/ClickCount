7b13e07b19e7372ba8bc582d55ae02833f04402f
iamqjin <iamqjin@gmail.com>
2017-01-18 21:59:18 +0900

    Storyboard 이용하

--- ClickCount/ViewController.swift
+++ ClickCount/ViewController.swift
@@ -12,32 +12,15 @@ class ViewController: UIViewController {
 
     var count = 0
     //옵셔널 벗김
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Label
-        var label = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        
-        self.view.addSubview(label)
-        self.label = label
-        
-        //Button
-        var button = UIButton()
-        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
-        
     }
     
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count = self.count + 1
         self.label.text = "\(self.count)"
     }
